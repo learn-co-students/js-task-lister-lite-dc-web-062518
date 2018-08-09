e86c875fba6151bf3efadcab65e0d3f51da579ad
finale100 <finale150@gmail.com>
2018-08-09 16:25:29 -0400

    done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,47 @@
+let commentCounter = 1
+let description = document.getElementById('new-task-description')
+let tasks = document.getElementById('tasks')
+let taskButton = document.getElementById('new-task')
+let deleteB = document.getElementsByClassName('button')
+let color = document.getElementById('colorChanger')
+let date = document.getElementById('date')
+let time = document.getElementById('time')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+init()
+
+function init() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const taskList = new TaskList();
+  });
+  taskButton.addEventListener('click', createTask)
+}
+
+
+function createTask(event) {
+  event.preventDefault()
+  li = document.createElement('li') // refactor to tasks.createElement
+  li.setAttribute('id', `task-${commentCounter}`)
+  li.appendChild(document.createTextNode(description.value + " on " + date.value + " " + time.value)) //setting li.text_value to input value(description.value)
+  tasks.appendChild(li)
+  colorChange(li)
+  createButton()
+  description.value = ""
+}
+
+function createButton() {
+  deleteButton = document.createElement('button')
+  deleteButton.setAttribute('id', `${commentCounter}`)
+  li.appendChild(deleteButton)
+  deleteButton.innerHTML = 'X'
+  deleteButton.addEventListener('click', deleteTask)
+  commentCounter++
+}
+
+function deleteTask(event) {
+  item = document.getElementById(`task-${event.target.id}`)
+  tasks.removeChild(item)
+}
+
+function colorChange(li) {
+  li.style.color = color.value
+}
